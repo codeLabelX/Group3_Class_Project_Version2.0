c6be3b2193a29cd4dca706cd031d89d2c68e53c4
krigora <ihor.kryvynskyy@icloud.com>
2021-10-05 00:14:38 -0500

    Create fiveFile

--- src/setup/FourthFile.java
+++
@@ -1,9 +0,0 @@
-package setup;
-
-public class FourthFile {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello Group3");
-    }
-}
---
+++ src/setup/fifthFile.java
@@ -0,0 +1,7 @@
+package setup;
+
+public class fifthFile {
+    public static void main(String[] args) {
+        System.out.println("Ihor");
+    }
+}
