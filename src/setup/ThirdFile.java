680eaa8e4aab61458bf878743d94fddc7e2e6148
nadiiapiekh <nadiapiekh@gmail.com>
2021-10-04 22:22:20 -0500

    Edited the third file

--- src/setup/ThirdFile.java
+++ src/setup/ThirdFile.java
@@ -3,5 +3,6 @@ package setup;
 public class ThirdFile {
     public static void main(String[] args) {
         System.out.println("This is out third file");
+        System.out.println("hello");
     }
 }
