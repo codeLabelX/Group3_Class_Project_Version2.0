18af57cddf1a8f4735fd4fde391e6919a5f45364
Daniel S <devolution.sdc@gmail.com>
2021-10-05 00:04:23 -0500

    does it work ?

--- src/setup/FirstFile.java
+++ src/setup/FirstFile.java
@@ -1,7 +1,7 @@
 package setup;
 
 public class FirstFile {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello world");
     }
 }
